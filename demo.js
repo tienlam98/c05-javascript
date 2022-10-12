a09dde8f8a782ac4c57e51589a3329d6a815db21
tienlam <lamtien98@gmail.com>
2022-10-13 01:17:46 +0700

    c05.03

--- demo.js
+++ demo.js
@@ -49,74 +49,124 @@
 // console.log(demSoUoc);
 
 // bai6a
-let x = Number(prompt("Nhap so nguyen bat ky"));
-function isPrime(number) {
-    if (number == 1) {
-        return true;
-    }
-    else {
-        if (number > 1) {
-            for (let i = 2; i < number; i++) {
-                if (number % i == 0) {
-                    return false;
-                }
-            }
-            return true;
-        }
-        else {
-            return false;
-        }
-    }
-}
-if (isPrime(x)) {
-    console.log(x, "la so nguyen to");
-}
-else {
-    console.log(x, "khong la so nguyen to");
-}
+// let x = Number(prompt("Nhap so nguyen bat ky"));
+// function isPrime(number) {
+//     if (number == 1) {
+//         return true;
+//     }
+//     else {
+//         if (number > 1) {
+//             for (let i = 2; i < number; i++) {
+//                 if (number % i == 0) {
+//                     return false;
+//                 }
+//             }
+//             return true;
+//         }
+//         else {
+//             return false;
+//         }
+//     }
+// }
+// if (isPrime(x)) {
+//     console.log(x, "la so nguyen to");
+// }
+// else {
+//     console.log(x, "khong la so nguyen to");
+// }
 
 // bai6b
 // let soUoc = 0;
 // let x = prompt("Nhap so nguyen bat ky");
-// if (x == 1) {
-//     console.log("so nguyen to");
-// }
-// else {
-//     for (let i = 1; i <= x; i++) {
-//         if (x % i == 0) {
-//             soUoc++;
-//         }
-//         if (soUoc == 2 && i == x) {
-//             console.log("Day la so nguyen so");
-//         }
-// }
+// for (let i = 1; i <= x; i++) {
+//     if (x % i == 0) {
+//         soUoc++;
+//         }    
+//     }
+// if (soUoc == 2) {
+//     console.log(x, "la so nguyen to");
 // }
 
 // bai7
-// let n = Number(prompt("Nhap so nguyen bat ky"));
-// for (let i = 0; i <= n; i++) {
-//     if (i % 2 == 0){
-//         console.log(i);
+// let a = [2,3,5,6,7,9];
+// let n = a.length;
+// for (let i = 0; i < n; i++) {
+//     if (a[i] % 2 == 0) {
+//         console.log(a[i]);
 //     }
 // }
 
 // bai8
-// let n = Number(prompt("Nhap so nguyen bat ky"));
-// for (let i = 0; i <= n; i++) {
-//     if (i > -1) {
-//         console.log(i);
+// let a = [1,3,5,7,-2,-7,10];
+// let n = a.length;
+// for (let i = 0; i < n; i++) {
+//     if (!(a[i] < 0)) {
+//         console.log(a[i]);
 //     }
 // }
 
 // bai9
+// let a = [1,3,5,7,-2,-7,10];
+// let soUoc = 0;
+// let n = a.length;
+// let flag = -1;
+// for (let i = 0; i < n; i++) {
+//     if (a[i] == 1) {
+//         flag = 1;
+//     }
+//     if (a[i] % )
+// }
 
 // bai10
 // let tongAm = 0;
-// let n = Number(prompt("Nhap so nguyen bat ky"));
-// for (let i = 0; i <= n; i++) {
-//     if (i > -1) {
-//         tongAm = tongAm + i;
+// let a = [1,2,3,-2,-4,-6,10];
+// let n = a.length;
+// for (let i = 0; i < n; i++) {
+//     if (a[i] < 0) {
+//         tongAm += a[i];
 //     }
 // }
-// console.log(tongAm);
+// console.log("Tong cac so am trong mang la", tongAm);
+
+// bai11
+
+// bai12
+// let demLe = 0;
+// let a = [1,2,3,7,8,22,13-2,-4,-3,-6,10];
+// let n = a.length;
+// for (let i = 0; i < n; i++) {
+//     if (a[i] % 2 != 0) {
+//         demLe++;
+//     }
+// }
+// console.log("Mang co",demLe, "so le");
+
+// bai13
+
+// bai14
+// let a = [1,2,3,7,-8,22,13-2,-4,-3,-6,10];
+// let n = a.length;
+// for (let i = 0; i < n; i++) {
+//     if (a[i] < 0) {
+//         console.log("So am dau tien la:", a[i]);
+//         break;
+//     }
+// }
+
+// bai15
+let maSinhVien = Number(prompt("Nhap ma so sinh vien"));
+let classA = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20]
+let n = classA.length;
+for (let i = 0; i < n; i++) {
+    if (classA[i] === maSinhVien) {
+        console.log("Ma so sinh vien o vi tri thu", i);
+    }
+}
+
+
+
+
+
+
+
 
